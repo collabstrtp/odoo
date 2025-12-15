d69f1a250d760eaca5d043ce7458b1d03afc30b4
govinda-mahanti <gmahanti955@gmail.com>
2025-12-15 13:58:46 +0530

    add

--- Backend/index.js
+++ Backend/index.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import { fileURLToPath } from "url";
 import path from "path";
-import connectDB from "../db/db.js";
+import connectDB from "./db/db.js";
 dotenv.config();
 
 import authRoutes from "./routes/authRoutes.js";
@@ -39,7 +39,7 @@ app.use("/categories", categoryRoutes);
 app.use("/approvalrules", approvalRuleRoutes);
 app.use("/approvalactions", approvalActionRoutes);
 app.use("/auditlogs", auditLogRoutes);
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
